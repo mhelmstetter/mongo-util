1754674064c316c0b8a657ebb11ceff4bfae1ce5
mhelmstetter <mark@helmstetter.com>
2019-11-26 09:10:57 -0800

    New corrupt worker

---
+++ src/main/java/com/mongodb/corruptutil/CorruptFinderWorker.java
@@ -0,0 +1,120 @@
+package com.mongodb.corruptutil;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.bson.BsonSerializationException;
+import org.bson.BsonValue;
+import org.bson.RawBsonDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+
+public class CorruptFinderWorker implements Runnable {
+
+    private static Logger logger = LoggerFactory.getLogger(CorruptFinderWorker.class);
+
+    private MongoCollection<RawBsonDocument> collection;
+    private MongoClient client;
+    FileOutputStream fos = null;
+    ZipOutputStream zipStream = null;
+    private File outDir;
+
+    public CorruptFinderWorker(MongoClient client, MongoCollection<RawBsonDocument> collection, File outDir) {
+        this.collection = collection;
+        this.client = client;
+        this.outDir = outDir;
+    }
+    
+    private void zip(String path, byte[] bytes) throws IOException {
+        if (fos == null) {
+            File outFile = new File(outDir, collection.getNamespace().getFullName()+".zip");
+            fos = new FileOutputStream(outFile);
+            zipStream = new ZipOutputStream(fos);
+        }
+        ZipEntry zipEntry = new ZipEntry(path);
+        zipStream.putNextEntry(zipEntry);
+        zipStream.write(bytes, 0, bytes.length);
+        zipStream.closeEntry();
+    }
+    
+    private void handleCorrupt(RawBsonDocument doc, BsonValue id) {
+        ByteBuffer buffer = doc.getByteBuffer().asNIO();
+        try {
+            zip(id.toString()+".bson", buffer.array());
+        } catch (IOException e) {
+            logger.error("Error adding to zip", e);
+        }
+    }
+
+    @Override
+    public void run() {
+        MongoCursor<RawBsonDocument> cursor = null;
+        long start = System.currentTimeMillis();
+        long last = start;
+        long count = 0;
+        long corruptCount = 0;
+
+        try {
+            cursor = collection.find().noCursorTimeout(true).iterator();
+            Number total = collection.countDocuments();
+            while (cursor.hasNext()) {
+                count++;
+                RawBsonDocument doc = cursor.next();
+                BsonValue id = null;
+                try {
+                    id = doc.get("_id");
+                } catch (Exception e) {
+                    corruptCount++;
+                    logger.trace(String.format("%s - Error reading doc id, count: %s, error: %s",
+                            collection.getNamespace(), count, e));
+                    handleCorrupt(doc, id);
+                    continue;
+                }
+
+                try {
+                    String s = doc.toJson();
+                } catch (BsonSerializationException bse) {
+                    corruptCount++;
+                    logger.trace(String.format("%s - Error serializing doc with _id: %s, count: %s, error: %s",
+                            collection.getNamespace(), id, count, bse));
+                    handleCorrupt(doc, id);
+                    continue;
+                }
+
+                long current = System.currentTimeMillis();
+                long delta = (current - last) / 1000;
+                if (delta >= 30) {
+                    logger.debug(String.format("%s - checked %s / %s documents, corruptCount: %s",
+                            collection.getNamespace(), count, total));
+                    last = current;
+                }
+            }
+
+        } finally {
+            cursor.close();
+            if (fos != null) {
+                try {
+                    zipStream.close();
+                    fos.close();
+                } catch (IOException e) {
+                    logger.debug("Error closing zip file", e);
+                }
+                
+            }
+        }
+        long end = System.currentTimeMillis();
+        Double dur = (end - start) / 1000.0;
+        logger.debug(String.format("Done validating %s, %s documents in %f seconds, corruptCount: %s",
+                collection.getNamespace(), count, dur, corruptCount));
+
+    }
+
+}
---
+++ src/main/java/com/mongodb/corruptutil/CorruptUtil.java
@@ -0,0 +1,136 @@
+package com.mongodb.corruptutil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.bson.RawBsonDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+
+public class CorruptUtil {
+    
+    private static Logger logger = LoggerFactory.getLogger(CorruptUtil.class);
+    
+    private Set<String> databasesBlacklist = new HashSet<>(Arrays.asList("system", "local", "config", "admin"));
+    private Set<String> collectionsBlacklist = new HashSet<>(Arrays.asList("system.indexes", "system.profile"));
+    
+    private static Options options;
+    
+    private MongoClient sourceClient;
+    private int threads = 4;
+    private File outDir;
+    
+    private ExecutorService executor;
+    
+    public CorruptUtil(String sourceUriStr) {
+        MongoClientURI source = new MongoClientURI(sourceUriStr);
+        sourceClient = new MongoClient(source);
+    }
+    
+    public void run() throws InterruptedException {
+        executor = Executors.newFixedThreadPool(threads);
+        MongoIterable<String> dbNames = sourceClient.listDatabaseNames();
+        for (String dbName : dbNames) {
+            if (! databasesBlacklist.contains(dbName)) {
+                MongoDatabase db = sourceClient.getDatabase(dbName);
+                logger.debug("db " + dbName);
+                MongoIterable<String> collectionNames = db.listCollectionNames();
+                for (String collectionName : collectionNames) {
+                    if (collectionsBlacklist.contains(collectionName)) {
+                        continue;
+                    }
+                    MongoCollection<RawBsonDocument> coll = db.getCollection(collectionName, RawBsonDocument.class);
+                    Runnable worker = new CorruptFinderWorker(sourceClient, coll, outDir);
+                    executor.execute(worker);
+                }
+            }
+        }
+        
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+            logger.debug("Waiting for pool to terminate");
+            Thread.sleep(1000);
+        }
+        logger.debug("CorruptUtil complete");
+    }
+    
+    private void setThreads(int threads) {
+        this.threads = threads;
+    }
+
+    @SuppressWarnings("static-access")
+    private static CommandLine initializeAndParseCommandLineOptions(String[] args) {
+        options = new Options();
+        options.addOption(new Option("help", "print this message"));
+        options.addOption(OptionBuilder.withArgName("Source cluster connection uri").hasArgs().withLongOpt("source")
+                .isRequired(true).create("s"));
+        options.addOption(OptionBuilder.withArgName("Zip output path").hasArgs().withLongOpt("outDir")
+                .isRequired(true).create("o"));
+        options.addOption(OptionBuilder.withArgName("# threads").hasArgs().withLongOpt("threads").create("t"));
+        
+
+        CommandLineParser parser = new GnuParser();
+        CommandLine line = null;
+        try {
+            line = parser.parse(options, args);
+            if (line.hasOption("help")) {
+                printHelpAndExit(options);
+            }
+        } catch (org.apache.commons.cli.ParseException e) {
+            System.out.println(e.getMessage());
+            printHelpAndExit(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+            printHelpAndExit(options);
+        }
+
+        return line;
+    }
+
+    private static void printHelpAndExit(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("logParser", options);
+        System.exit(-1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        CommandLine line = initializeAndParseCommandLineOptions(args);
+        CorruptUtil util = new CorruptUtil(line.getOptionValue("s"));
+        String threadsStr = line.getOptionValue("t");
+        if (threadsStr != null) {
+            int threads = Integer.parseInt(threadsStr);
+            util.setThreads(threads);
+        }
+        String outDirStr = line.getOptionValue("o");
+        File outDir = new File(outDirStr);
+        if (! outDir.canWrite()) {
+            throw new IOException("Can't write to outDir " + outDirStr);
+        }
+        util.setOutDir(outDir);
+        util.run();
+
+    }
+
+    private void setOutDir(File outDir) {
+        this.outDir = outDir;
+    }
+
+}
