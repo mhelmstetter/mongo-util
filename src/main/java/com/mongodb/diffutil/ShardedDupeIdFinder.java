38b1999e540697bd3d35cf8b41cee76b3d0f776c
mhelmstetter <mark@helmstetter.com>
2020-04-17 12:51:15 -0700

    Refacotred dupe finder, split by ObjectId date ranges

--- src/main/java/com/mongodb/diffutil/ShardedDupeIdFinder.java
+++ src/main/java/com/mongodb/diffutil/ShardedDupeIdFinder.java
@@ -1,7 +1,10 @@
 package com.mongodb.diffutil;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +23,7 @@ import org.apache.commons.cli.Options;
 import org.bson.BsonValue;
 import org.bson.Document;
 import org.bson.RawBsonDocument;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +31,6 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
-import com.mongodb.model.Namespace;
 import com.mongodb.shardsync.ShardClient;
 
 public class ShardedDupeIdFinder {
@@ -48,22 +51,19 @@ public class ShardedDupeIdFinder {
 	long totalMissingDocs = 0;
 	long totalKeysMisordered = 0;
 	long totalHashMismatched = 0;
-	
+
 	private Set<BsonValue> ids;
-	
-    private Set<String> databasesBlacklist = new HashSet<>(Arrays.asList("system", "local", "config", "admin"));
-    private Set<String> collectionsBlacklist = new HashSet<>(Arrays.asList("system.indexes", "system.profile"));
-    
-    private ExecutorService executor;
+
+	private Set<String> databasesBlacklist = new HashSet<>(Arrays.asList("system", "local", "config", "admin"));
+	private Set<String> collectionsBlacklist = new HashSet<>(Arrays.asList("system.indexes", "system.profile"));
+
+	private ExecutorService executor;
 
 	@SuppressWarnings("unchecked")
 	public ShardedDupeIdFinder(String sourceClusterUri) {
 		this.sourceClusterUri = sourceClusterUri;
 		sourceShardClient = new ShardClient("source", sourceClusterUri);
 		sourceShardClient.populateShardMongoClients();
-
-		
-
 		Document listDatabases = new Document("listDatabases", 1);
 		Document sourceDatabases = sourceShardClient.adminCommand(listDatabases);
 
@@ -77,40 +77,60 @@ public class ShardedDupeIdFinder {
 		}
 	}
 
-	
+	private Document agg(String dbName, String collectionName) {
+		MongoClient mongosClient = sourceShardClient.getMongoClient();
+		MongoDatabase db = mongosClient.getDatabase(dbName);
+		MongoCollection<Document> coll = db.getCollection(collectionName);
+
+		Document d = Document.parse("{'$group': {'_id': null, 'max': { '$max': '$_id' }, 'min': { '$min': '$_id' }}}");
+		List<Document> pipeline = new ArrayList<Document>();
+		pipeline.add(d);
+		Document aggResult = coll.aggregate(pipeline).first();
+		return aggResult;
+	}
+
 	public void run() throws InterruptedException {
-        executor = Executors.newFixedThreadPool(sourceShardClient.getShardMongoClients().size());
-        
-		
-		for (Map.Entry<String, MongoClient> entry : sourceShardClient.getShardMongoClients().entrySet()) {
-            MongoClient sourceClient = entry.getValue();
-            String shardName = entry.getKey();
-        	
-        	for (String dbName : sourceDbInfoMap.keySet()) {
-                if (! databasesBlacklist.contains(dbName)) {
-                    MongoDatabase db = sourceClient.getDatabase(dbName);
-                    logger.debug("db " + dbName);
-                    MongoIterable<String> collectionNames = db.listCollectionNames();
-                    for (String collectionName : collectionNames) {
-                        if (collectionsBlacklist.contains(collectionName)) {
-                            continue;
-                        }
-                        Set<BsonValue> syncSet = Collections.synchronizedSet(new HashSet<>());
-        				MongoCollection<RawBsonDocument> sourceColl = db.getCollection(collectionName,
-        						RawBsonDocument.class);
-                        Runnable worker = new ShardedDupeIdFinderWorker(sourceClient, sourceColl, syncSet, shardName);
-                        executor.execute(worker);
-                    }
-                }
-        	}
-        }
-        
-        executor.shutdown();
-        while (!executor.isTerminated()) {
-            Thread.sleep(10000);
-        }
-        logger.debug("ShardedDupeIdFinder complete");
-    }
+		executor = Executors.newFixedThreadPool(sourceShardClient.getShardMongoClients().size());
+
+		for (String dbName : sourceDbInfoMap.keySet()) {
+			if (!databasesBlacklist.contains(dbName)) {
+				MongoDatabase db = sourceShardClient.getMongoClient().getDatabase(dbName);
+				logger.debug("db " + dbName);
+				MongoIterable<String> collectionNames = db.listCollectionNames();
+				for (String collectionName : collectionNames) {
+					if (collectionsBlacklist.contains(collectionName)) {
+						continue;
+					}
+					Document agg = agg(dbName, collectionName);
+					logger.debug("agg: " + agg);
+					ObjectId min = agg.getObjectId("min");
+					Date minDate = min.getDate();
+					ObjectId max = agg.getObjectId("max");
+					Date maxDate = max.getDate();
+
+					Calendar myCal = Calendar.getInstance();
+					myCal.setTime(minDate);
+
+					while (myCal.getTime().before(maxDate)) {
+						Date d1 = myCal.getTime();
+						myCal.add(Calendar.MONTH, +1);
+						Date d2 = myCal.getTime();
+						logger.debug(d1 + " - " + d2);
+
+						Runnable worker = new ShardedDupeIdFinderWorker(sourceShardClient, dbName, collectionName, d1,
+								d2);
+						executor.execute(worker);
+					}
+				}
+			}
+		}
+		executor.shutdown();
+		while (!executor.isTerminated()) {
+			Thread.sleep(10000);
+		}
+		logger.debug("ShardedDupeIdFinder complete");
+	}
+
 
 	@SuppressWarnings("static-access")
 	private static CommandLine initializeAndParseCommandLineOptions(String[] args) {
