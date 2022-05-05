326d2464cffcd75b223012f7989b91ae9594f462
Mark Helmstetter <mark@helmstetter.com>
2022-05-05 13:21:45 -0700

    Format bson types as strings

--- src/main/java/com/mongodb/mongoexport/MongoExport.java
+++ src/main/java/com/mongodb/mongoexport/MongoExport.java
@@ -1,8 +1,22 @@
 package com.mongodb.mongoexport;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.PrintStream;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 import java.util.concurrent.Callable;
 
 import org.bson.Document;
+import org.bson.UuidRepresentation;
+import org.bson.json.JsonMode;
+import org.bson.json.JsonWriterSettings;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
@@ -26,6 +40,12 @@ public class MongoExport implements Callable<Integer> {
 	@Option(names = {"-c", "--collection"}, description = "collection to use", required = true)
     private String collectionName;
 	
+	@Option(names = {"-d", "--db"}, description = "database to use", required = true)
+    private String dbName;
+	
+	@Option(names = {"-o", "--out"}, description = "output file; if not specified, stdout is used", required = false)
+    private String outFile;
+	
 	@Override
     public Integer call() throws Exception { 
         
@@ -35,30 +55,48 @@ public class MongoExport implements Callable<Integer> {
                 .build();
 		MongoClient mongoClient = MongoClients.create(mongoClientSettings);
 		
-		String dbName = connectionString.getDatabase();
+		JsonWriterSettings settings = getJsonWriterSettings();
+		
 		MongoDatabase db = mongoClient.getDatabase(dbName);
 		//mongoClient.getDatabase("admin").runCommand(new Document("ping", 1));
 		MongoCursor<Document> cursor = null;
+		
+		BufferedWriter bw = null;
+		if (outFile != null) {
+			File file = new File(outFile);
+			FileWriter fw = new FileWriter(file);
+			  bw = new BufferedWriter(fw);
+		}
+		
+		PrintStream os = null;
+		
         long start = System.currentTimeMillis();
         long count = 0;
         try {
+        	
+        	if (outFile != null) {
+        		os = new PrintStream(new BufferedOutputStream(new FileOutputStream(outFile)) );
+        	} else {
+        		os = new PrintStream(new BufferedOutputStream(System.out) );
+        	}
+        	
             
             MongoCollection<Document> mongoCollection = db.getCollection(collectionName);
-            long totalDocs = mongoCollection.countDocuments();
+            long totalDocs = mongoCollection.estimatedDocumentCount();
             cursor = mongoCollection.find().iterator();
             
             while (cursor.hasNext()) {
                 Document doc = cursor.next();
-                System.out.println(doc.toJson());
+                os.println(doc.toJson(settings));
                 count++;
-                if (count % 100000 == 0) {
-                    double complete = count/totalDocs*100.0;
-                    //System.out.println(count + "/" + totalDocs + "(" + complete + ")");
-                    System.err.println(String.format("%s / %s  (%f)", count, totalDocs, count/totalDocs*100.0));
+                if (count % 10000 == 0) {
+                    double complete = ((double)count / (double)totalDocs) *100.0;
+                    System.out.println(String.format("%s / %s  (%5.1f %%)", count, totalDocs, complete));
                 }
             }
         } finally {
             cursor.close();
+            os.close();
         }
         long end = System.currentTimeMillis();
         Double dur = (end - start)/1000.0;
@@ -66,6 +104,37 @@ public class MongoExport implements Callable<Integer> {
 		
         return 0;
     }
+	
+	private JsonWriterSettings getJsonWriterSettings() {
+		return JsonWriterSettings.builder()
+		        .outputMode(JsonMode.RELAXED)
+		        .binaryConverter(
+		            (value, writer) -> {
+		            	if (value.getType() == 4) {
+		            		writer.writeString(value.asUuid().toString());
+		            	} else if (value.getType() == 3) {
+		            		writer.writeString(value.asUuid(UuidRepresentation.JAVA_LEGACY).toString());
+		            	} else {
+		            		writer.writeString(Base64.getEncoder().encodeToString(value.getData()));
+		            	}
+		            	
+		            })
+		        .dateTimeConverter(
+		            (value, writer) -> {
+		              ZonedDateTime zonedDateTime = Instant.ofEpochMilli(value).atZone(ZoneOffset.UTC);
+		              writer.writeString(DateTimeFormatter.ISO_DATE_TIME.format(zonedDateTime));
+		            })
+		        .decimal128Converter((value, writer) -> writer.writeString(value.toString()))
+		        .objectIdConverter((value, writer) -> writer.writeString(value.toHexString()))
+		        .symbolConverter((value, writer) -> writer.writeString(value))
+		        .timestampConverter((value, writer) -> {
+		        	writer.writeStartObject();
+		        	writer.writeNumber("t", String.valueOf(value.getTime()));
+		        	writer.writeNumber("t", String.valueOf(value.getInc()));
+		        	writer.writeEndObject();
+		        })
+		        .build();
+	}
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new MongoExport()).execute(args);
