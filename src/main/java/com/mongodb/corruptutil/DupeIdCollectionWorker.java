68cfb81d74e015d79506f1ea5f3a15c6fd6e3f34
Mark Helmstetter <mark@helmstetter.com>
2024-07-15 22:00:41 -0700

    Fixing several issues

--- src/main/java/com/mongodb/corruptutil/DupeIdCollectionWorker.java
+++ src/main/java/com/mongodb/corruptutil/DupeIdCollectionWorker.java
@@ -5,9 +5,7 @@ import static com.mongodb.client.model.Filters.gte;
 import static com.mongodb.client.model.Projections.include;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorCompletionService;
@@ -26,31 +24,19 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Aggregates;
-import com.mongodb.client.model.BulkWriteOptions;
 import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.Sorts;
-import com.mongodb.client.model.WriteModel;
 import com.mongodb.util.CallerBlocksPolicy;
 
-import io.netty.util.concurrent.Future;
-
 public class DupeIdCollectionWorker implements Runnable {
-	
-	private final static int ONE_MINUTE = 60 * 1000;
+    
+    private final static int ONE_MINUTE = 60 * 1000;
 
     private static Logger logger = LoggerFactory.getLogger(DupeIdCollectionWorker.class);
 
     private MongoCollection<RawBsonDocument> collection;
     
     private MongoDatabase archiveDb;
-    
-    private final static int BATCH_SIZE = 1000;
-    
-    private final static BulkWriteOptions bulkWriteOptions = new BulkWriteOptions().ordered(false);
-    
-    private Map<String, List<WriteModel<RawBsonDocument>>> writeModelsMap = new HashMap<>();
-    
-    private List<BsonValue> dupesBatch = new ArrayList<>(BATCH_SIZE);
     
     private String base;
     
@@ -59,11 +45,11 @@ public class DupeIdCollectionWorker implements Runnable {
     Bson sort = eq("_id", 1);
     
     private final int queueSize = 25000;
-	private final int threads = 4;
+    private final int threads = 4;
     BlockingQueue<Runnable> workQueue;
     
-	protected ThreadPoolExecutor executor = null;
-	private ExecutorCompletionService<DupeIdTaskResult> completionService;
+    protected ThreadPoolExecutor executor = null;
+    private ExecutorCompletionService<DupeIdTaskResult> completionService;
 
     public DupeIdCollectionWorker(MongoCollection<RawBsonDocument> collection, MongoDatabase archiveDb, Integer startId) {
         this.collection = collection;
@@ -72,15 +58,13 @@ public class DupeIdCollectionWorker implements Runnable {
         this.base = collection.getNamespace().getFullName();
         
         workQueue = new ArrayBlockingQueue<>(queueSize);
-		executor = new ThreadPoolExecutor(threads, threads, 30, TimeUnit.SECONDS, workQueue, new CallerBlocksPolicy(ONE_MINUTE*5));
-		completionService = new ExecutorCompletionService<>(executor);
+        executor = new ThreadPoolExecutor(threads, threads, 30, TimeUnit.SECONDS, workQueue, new CallerBlocksPolicy(ONE_MINUTE*5));
+        completionService = new ExecutorCompletionService<>(executor);
     }
     
-
-
     @Override
     public void run() {
-    	
+        
         MongoCursor<RawBsonDocument> cursor = null;
         long start = System.currentTimeMillis();
         long count = 0;
@@ -88,44 +72,41 @@ public class DupeIdCollectionWorker implements Runnable {
         long submitCount = 0;
 
         try {
-        	Bson proj = include("_id");
-    		Bson query = null;
-    		if (startId != null) {
-    			query = gte("_id", startId);
-    			cursor = collection.find(query).projection(proj).sort(sort).iterator();
-    		} else {
-    			 cursor = collection.find().projection(proj).sort(sort).iterator();
-    		}
-    		
+            Bson proj = include("_id");
+            Bson query = null;
+            if (startId != null) {
+                query = gte("_id", startId);
+                cursor = collection.find(query).projection(proj).sort(sort).iterator();
+            } else {
+                cursor = collection.find().projection(proj).sort(sort).iterator();
+            }
+            
             logger.debug("starting worker query {}", collection.getNamespace());
             Number total = collection.estimatedDocumentCount();
             
             logger.debug("{} estimated doc count: {}", collection.getNamespace(), total);
-            
-            
             
             List<Bson> pipeline = new ArrayList<>();
             pipeline.add(Aggregates.project(Projections.fields(Projections.include("_id"))));
             pipeline.add(Aggregates.sample(25000));
             pipeline.add(Aggregates.sort(Sorts.orderBy(Sorts.ascending("_id"))));
             
-            
             AggregateIterable<RawBsonDocument> results = collection.aggregate(pipeline);
             
             BsonValue last = null;
             for (BsonDocument result : results) {
-            	BsonValue id = result.get("_id");
-            	DupeIdTask task = new DupeIdTask(collection, archiveDb, last, id);
-            	completionService.submit(task);
-            	submitCount++;
-            	last = id;
+                BsonValue id = result.get("_id");
+                DupeIdTask task = new DupeIdTask(collection, archiveDb, last, id);
+                completionService.submit(task);
+                submitCount++;
+                last = id;
             }
             logger.debug("submitted {} tasks", submitCount);
                  
             for (int i = 0; i < submitCount; i++) {
-            	DupeIdTaskResult result = completionService.take().get();
-            	dupeCount += result.dupeCount;
-            	count += result.totalCount;
+                DupeIdTaskResult result = completionService.take().get();
+                dupeCount += result.dupeCount;
+                count += result.totalCount;
             }
             
         } catch (Exception e) {
@@ -139,5 +120,4 @@ public class DupeIdCollectionWorker implements Runnable {
                 collection.getNamespace(), count, dur, dupeCount));
 
     }
-
 }
--- src/main/java/com/mongodb/corruptutil/DupeIdTask.java
+++ src/main/java/com/mongodb/corruptutil/DupeIdTask.java
@@ -9,8 +9,10 @@ import static com.mongodb.client.model.Projections.include;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 import org.bson.BsonValue;
@@ -30,59 +32,54 @@ import com.mongodb.client.model.WriteModel;
 
 public class DupeIdTask implements Callable<DupeIdTaskResult> {
 
-	private static Logger logger = LoggerFactory.getLogger(DupeIdTask.class);
-	private final static BulkWriteOptions bulkWriteOptions = new BulkWriteOptions().ordered(false);
-	
-	private final static Bson sort = eq("_id", 1);
-	private final static Bson proj = include("_id");
-	
-	private MongoCollection<RawBsonDocument> collection;
-	private MongoDatabase archiveDb;
-	private String base;
-	private List<BsonValue> dupesBatch;
-	private Map<String, List<WriteModel<RawBsonDocument>>> writeModelsMap = new HashMap<>();
-	
-	BsonValue startId;
-	BsonValue endId;
-	
+    private static Logger logger = LoggerFactory.getLogger(DupeIdTask.class);
+    private final static BulkWriteOptions bulkWriteOptions = new BulkWriteOptions().ordered(false);
+    
+    private final static Bson sort = eq("_id", 1);
+    private final static Bson proj = include("_id");
+    
+    private MongoCollection<RawBsonDocument> collection;
+    private MongoDatabase archiveDb;
+    private String base;
+    private List<BsonValue> dupesBatch = new ArrayList<>();
+    private Map<String, List<WriteModel<RawBsonDocument>>> writeModelsMap = new HashMap<>();
+    
+    BsonValue startId;
+    BsonValue endId;
+    
+    private final static int BATCH_SIZE = 1000;
 
-	private final static int BATCH_SIZE = 1000;
+    public DupeIdTask(MongoCollection<RawBsonDocument> collection, MongoDatabase archiveDb, BsonValue startId, BsonValue endId) {
+        this.collection = collection;
+        this.archiveDb = archiveDb;
+        this.base = collection.getNamespace().getFullName();
+        this.startId = startId;
+        this.endId = endId;
+    }
 
-	public DupeIdTask(MongoCollection<RawBsonDocument> collection, MongoDatabase archiveDb, BsonValue startId, BsonValue endId) {
-		this.collection = collection;
-		this.archiveDb = archiveDb;
-		this.base = collection.getNamespace().getFullName();
-		this.startId = startId;
-		this.endId = endId;
-	}
-
-	@Override
-	public DupeIdTaskResult call() throws Exception {
-		
-		MongoCursor<RawBsonDocument> cursor = null;
-       
+    @Override
+    public DupeIdTaskResult call() throws Exception {
+        
+        MongoCursor<RawBsonDocument> cursor = null;
         long count = 0;
         long dupeCount = 0;
-        
         BsonValue lastId = null;
+        List<RawBsonDocument> duplicateDocs = new ArrayList<>();
         
-		try {
-			
-			
-    		Bson query = null;
-    		
-    		if (startId == null) {
-    			query = lt("_id", endId);
-    		} else if (endId == null) {
-    			query = gte("_id", startId);
-    		} else {
-    			query = and(gte("_id", startId), lt("_id", endId));
-    		}
-    		
-    		
-    		cursor = collection.find(query).projection(proj).sort(sort).iterator();
-
-			while (cursor.hasNext()) {
+        try {
+            Bson query = null;
+            
+            if (startId == null) {
+                query = lt("_id", endId);
+            } else if (endId == null) {
+                query = gte("_id", startId);
+            } else {
+                query = and(gte("_id", startId), lt("_id", endId));
+            }
+            
+            cursor = collection.find(query).projection(proj).sort(sort).iterator();
+            
+            while (cursor.hasNext()) {
                 count++;
                 RawBsonDocument doc = cursor.next();
                 BsonValue id = null;
@@ -95,120 +92,98 @@ public class DupeIdTask implements Callable<DupeIdTaskResult> {
                 }
                 
                 if (id.equals(lastId)) {
-                	handleDupes(doc);
-                	dupeCount++;
-                	logger.warn("{} - duplicate _id found for _id: {}", collection.getNamespace(), id);
+                    duplicateDocs.add(doc);
+                    dupeCount++;
+                    logger.warn("{} - duplicate _id found for _id: {}", collection.getNamespace(), id);
+                } else {
+                    if (!duplicateDocs.isEmpty()) {
+                        handleDupes(duplicateDocs);
+                        duplicateDocs.clear();
+                    }
                 }
                 lastId = id;
-                //lastDocument = doc;
             }
-			
-		} catch (Exception e) {
-			logger.error("call error", e);
-		} finally {
-			cursor.close();
+            
+            // Handle any remaining duplicates
+            if (!duplicateDocs.isEmpty()) {
+                handleDupes(duplicateDocs);
+            }
+        } catch (Exception e) {
+            logger.error("call error", e);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
             flushAll();
-		}
-		return new DupeIdTaskResult(count, dupeCount);
-	}
-	
-	private void handleDupes(RawBsonDocument doc) {
-    	if (archiveDb != null) {
-    		
-    		dupesBatch.add(doc.get("_id"));
-    		if (dupesBatch.size() >= BATCH_SIZE) {
-    			
-    			Bson query = in("_id", dupesBatch);
-        		int d = 1;
-        		MongoCursor<RawBsonDocument> cursor = collection.find(query).sort(sort).iterator();
-        		
-        		BsonValue lastId = null;
-        		
-        		while (cursor.hasNext()) {
-                    RawBsonDocument fullDoc = cursor.next();
-                    BsonValue id = null;
-                    try {
-                        id = doc.get("_id");
-                    } catch (Exception e) {
-                        logger.warn(String.format("%s - Error reading doc id, fullDoc: %s, error: %s",
-                                collection.getNamespace(), fullDoc, e));
-                        continue;
-                    }
-                    
-                    if (id.equals(lastId)) {
-                    	handleDupes(doc);
-                    	logger.warn("{} - duplicate _id found for _id: {}", collection.getNamespace(), id);
-                    	
-                    	String collName = String.format("%s_%s", base, d++);
-        				insert(fullDoc, collName);
-                    } else {
-                    	d = 1;
-                    }
-                    lastId = id;
-        		}
-        		
-        		dupesBatch.clear();
-        		
-    		}
-    	}
-    	
+        }
+        return new DupeIdTaskResult(count, dupeCount);
     }
+    
+    private void handleDupes(List<RawBsonDocument> docs) {
+        if (archiveDb != null) {
+            Set<BsonValue> processedIds = new HashSet<>();
+            int d = 1;
 
-	private void flushDupes(List<RawBsonDocument> dupesBuffer) {
-		if (dupesBuffer.size() > 1) {
-			int i = 1;
-			for (RawBsonDocument dupe : dupesBuffer) {
-				String collName = String.format("%s_%s", base, i++);
-				insert(dupe, collName);
-			}
-		} else if (dupesBuffer.size() == 1) {
-			BsonValue id = dupesBuffer.get(0).get("_id");
-			logger.warn("duplicate from manifest not found as duplicate for _id: {}", id);
-		}
-		dupesBuffer.clear();
-	}
+            for (RawBsonDocument doc : docs) {
+                BsonValue docId = doc.get("_id");
 
-	private void insert(RawBsonDocument fullDoc, String collName) {
-		WriteModel<RawBsonDocument> model = new InsertOneModel<>(fullDoc);
+                if (!processedIds.add(docId)) {
+                    // This id has already been processed, continue to the next one
+                    continue;
+                }
 
-		List<WriteModel<RawBsonDocument>> writeModels = writeModelsMap.get(collName);
-		if (writeModels == null) {
-			writeModels = new ArrayList<>(BATCH_SIZE);
-			writeModelsMap.put(collName, writeModels);
-		}
+                dupesBatch.add(docId);
 
-		writeModels.add(model);
-	}
+                if (dupesBatch.size() >= BATCH_SIZE) {
+                    Bson query = in("_id", dupesBatch);
+                    MongoCursor<RawBsonDocument> cursor = collection.find(query).sort(sort).iterator();
+                    
+                    try {
+                        BsonValue lastId = null;
+                        while (cursor.hasNext()) {
+                            RawBsonDocument fullDoc = cursor.next();
+                            BsonValue id = fullDoc.get("_id");
 
-	private void flushInternal(String collName, List<WriteModel<RawBsonDocument>> writeModels) {
-		if (writeModels.size() == 0) {
-			return;
-		}
-		BulkWriteResult bulkWriteResult = null;
-		try {
-			MongoCollection<RawBsonDocument> c1 = archiveDb.getCollection(collName, RawBsonDocument.class);
-			bulkWriteResult = c1.bulkWrite(writeModels, bulkWriteOptions);
-		} catch (MongoBulkWriteException err) {
-			// List<BulkWriteError> errors = err.getWriteErrors();
-			bulkWriteResult = err.getWriteResult();
-			logger.warn("bulk write errors, insertedCount: {}, errorCount: {}", bulkWriteResult.getInsertedCount(),
-					err.getWriteErrors().size());
-			logger.error("bulk write error", err);
-		} catch (Exception ex) {
-			logger.error("{} unknown error: {}", ex.getMessage(), ex);
-		} finally {
-			writeModels.clear();
-		}
-	}
-
-	private void flushAll() {
-
-		for (Map.Entry<String, List<WriteModel<RawBsonDocument>>> entry : writeModelsMap.entrySet()) {
-			String collName = entry.getKey();
-			List<WriteModel<RawBsonDocument>> writeModels = entry.getValue();
-			flushInternal(collName, writeModels);
-		}
-
-	}
-
+                            if (id.equals(lastId)) {
+                                logger.warn("{} - duplicate _id found for _id: {}", collection.getNamespace(), id);
+                                String collName = String.format("%s_%s", base, d++);
+                                insert(fullDoc, collName);
+                            } else {
+                                d = 1;
+                            }
+                            lastId = id;
+                        }
+                    } catch (Exception e) {
+                        logger.error("Error processing duplicates", e);
+                    } finally {
+                        cursor.close();
+                    }
+                    dupesBatch.clear();
+                }
+            }
+        }
+    }
+    
+    private void insert(RawBsonDocument doc, String collName) {
+        List<WriteModel<RawBsonDocument>> models = writeModelsMap.computeIfAbsent(collName, k -> new ArrayList<>());
+        models.add(new InsertOneModel<>(doc));
+        if (models.size() >= BATCH_SIZE) {
+            flush(collName, models);
+        }
+    }
+    
+    private void flush(String collName, List<WriteModel<RawBsonDocument>> models) {
+        try {
+            MongoCollection<RawBsonDocument> coll = archiveDb.getCollection(collName, RawBsonDocument.class);
+            BulkWriteResult result = coll.bulkWrite(models, bulkWriteOptions);
+            logger.info("flushed {} records to {}", result.getInsertedCount(), collName);
+        } catch (MongoBulkWriteException e) {
+            logger.error("Bulk write error", e);
+        }
+        models.clear();
+    }
+    
+    private void flushAll() {
+        writeModelsMap.forEach(this::flush);
+    }
 }
