2ff97327f9026a1d0bb45104d4745fc652681643
Mark Helmstetter <mark@helmstetter.com>
2025-01-31 15:34:35 -0800

    Rename old log file before writing new one

--- src/main/java/com/mongodb/mongosync/MongoSyncLogHandler.java
+++ src/main/java/com/mongodb/mongosync/MongoSyncLogHandler.java
@@ -4,13 +4,20 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.regex.Pattern;
 
 import org.apache.commons.exec.LogOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MongoSyncLogHandler extends LogOutputStream {
+	
+	protected static final Logger logger = LoggerFactory.getLogger(MongoSync.class);
+	
     private MongoSyncEventListener listener;
     
     private static final String[] READY_FOR_PAUSE_PHRASES = new String[] {
@@ -27,7 +34,21 @@ public class MongoSyncLogHandler extends LogOutputStream {
         super();
         this.listener = listener;
         readyForPauseRegex = constructRegexContainingPhrases(READY_FOR_PAUSE_PHRASES);
-        writer = new PrintWriter(new FileWriter(new File(logPath, id + ".log")));
+        
+        File logFile = new File(logPath, id + ".log");
+        if (logFile.exists()) {
+            // Get current date and time
+            String timeStamp = new SimpleDateFormat("yyyy-MM-dd:HH:mm").format(new Date());
+            // Create a new file name with the date appended
+            File renamedFile = new File(logPath, id + "_" + timeStamp + ".log");
+            if (logFile.renameTo(renamedFile)) {
+                logger.debug("Renamed existing log file to: {}", renamedFile.getName());
+            } else {
+                logger.warn("Failed to rename existing log file.");
+            }
+        }
+        
+        writer = new PrintWriter(new FileWriter(logFile));
     }
     
     private void matcher(String line) {
